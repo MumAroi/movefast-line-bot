afb96d8ece97490bbe5f8198b46d18419f76c663
Paramest <paramest.wae@movefast.me>
2019-02-25 16:29:03 +0700

    test

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
     text = event.message.text #message from user
-    print( event.reply_token)
+    print(text)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=text)) #reply the same message from user

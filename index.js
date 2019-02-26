935886e46cc0036361f04b404bc864747d828c81
Paramest <paramest.wae@movefast.me>
2019-02-26 11:29:40 +0700

    is ok

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require('express');
 // create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET
+  channelSecret: process.env.CHANNEL_SECRET 
 };
 
 // create LINE SDK client

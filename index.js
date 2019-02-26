da276b85c9bb062731b790f05f1192b95dc52363
Paramest <paramest.wae@movefast.me>
2019-02-26 11:17:34 +0700

    test

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ app.get('/', (req,res)=>{
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
 app.post('/callback', line.middleware(config), (req, res) => {
-  console.log(req.body.events)
   Promise
     .all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
@@ -44,7 +43,6 @@ function handleEvent(event) {
     event.replyToken === 'ffffffffffffffffffffffffffffffff') {
     return;
   }
-  console.log('pass')
 
   // create a echoing text message
   const echo = { type: 'text', text: event.message.text };
